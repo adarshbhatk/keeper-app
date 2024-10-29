c1d5074290d017c7c8a94d3e8d9ed855ffb6ff44
Adarsh K <adarshbhatk@gmail.com>
2024-10-29 12:30:32 +0530

    Add React props to update note dynamically

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import Heading from "./Heading";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
+
+function createNote(note) {
+  return (
+    <Note 
+    key={note.key}
+    title={note.title}
+    content={note.content}
+    />
+  );
+}
 
 function App() {
     return (
         <div className="body">
             <Heading />
-            <Note />
+            {notes.map(createNote)}
             <Footer />
         </div>
     );
